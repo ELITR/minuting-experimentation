85224004734114a4e5d7d0f270db9860fd0d39c6
erionc <g-user1@outlook.com>
2020-02-03 13:29:30 +0100

    Added supervised segmentation method

--- basic_seg/super_seg.py
+++ basic_seg/super_seg.py
@@ -2,8 +2,6 @@
 '''
 Supervised text segmentation describet at 
 https://arxiv.org/abs/1803.09337
-and available at 
-https://github.com/koomri/text-segmentation
 '''
 
 import os, sys, codecs, re, string, nltk, argparse
