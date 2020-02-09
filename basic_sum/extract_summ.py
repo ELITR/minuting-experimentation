05cdc6cbee6dcd1f67cb97013ea4d6306d5a3099
erionc <g-user1@outlook.com>
2020-02-09 11:49:32 +0100

    Exploring Unsupervised Extractive TS

--- basic_sum/extract_summ.py
+++ basic_sum/extract_summ.py
@@ -2,7 +2,9 @@
 '''
 Supervised extractive text summarizer that is used to extract the most
 important conversation utterances, especially those that reflect decisions
-that were drawn from the meeting discussions.
+that were drawn from the meeting discussions. 
+repo1:  https://github.com/hongwang600/Summarization
+paper1: https://arxiv.org/abs/1906.04466
 '''
 
 import os, sys, re, string, argparse, nltk
